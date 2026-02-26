5955ceda359052aef5ba7ead5a780bb632d17713
Alessandro Maurini <maurini.alessandro.23@itisriva.edu.it>
2026-02-26 21:59:38 +0100

    implementazione punto 4 e punto 3 in fase di sviluppo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,9 +13,6 @@ namespace AgendaPersonale
             Console.WriteLine("GOOGLE CALENDAR NON PLUS ULTRA");
 
             string[] attivita = new string[100];
-            /* FORMATO DELLE ATTIVITA': Data | Titolo | Descrizione
-             * Formato di una data: YYYY.MM.DD-HH:mm
-             */
 
             bool continua = true;
             while (continua)
@@ -26,39 +23,58 @@ namespace AgendaPersonale
                 switch (scelta)
                 {
                     case "1":
-                        // Visualizzazione delle attività inserite, una per riga
-                        // non in ordine cronologico, ma per come sono presenti nell'array
                         visualizza(attivita);
                         break;
+
                     case "2":
-                        /* INSERIMENTO DI UNA NUOVA ATTIVITA' in coda all'array
-                         * Usare il formato richiesto ma richiedere ogni dato separatamente
-                         * ATTENZIONE: non cancellare le attività già inserite */
                         Console.Write("Inserire il titolo dell'attività: ");
                         string titolo = Console.ReadLine();
                         Console.Write("Inserire la descrizione dell'attività: ");
                         string descrizione = Console.ReadLine();
                         Console.Write("Inserire l'anno dell'attività: ");
                         int anno = int.Parse(Console.ReadLine());
-                        Console.Write("Inserire il mese dell'attività: ");
-                        int mese = int.Parse(Console.ReadLine()); // DA FARE: non andare avanti fino a che il mese non è corretto
+                        Console.Write("Inserire il mese dell'attività a 2 cifre: ");
+                        int mese = int.Parse(Console.ReadLine()); // Idea: string meseFormattato = mese.ToString("00");
+                        bool meseValido = false;
+                        while (!meseValido)
+                        {
+                            if (mese >= 1 && mese <= 12)
+                            {
+                                meseValido = true;
+                            }
+                            else
+                            {
+                                Console.Write("Mese non valido. Inserire un mese compreso tra 1 e 12 a 2 cifre: ");
+                                mese = int.Parse(Console.ReadLine());
+                            }
+                        }
                         Console.Write("Inserire il giorno dell'attività: ");
-                        int giorno = int.Parse(Console.ReadLine()); // DA FARE: non andare avanti fino a che il giorno non è corretto
+                        int giorno = int.Parse(Console.ReadLine());
+                        bool giornoValido = false;
+                        while (!giornoValido) // Idea: se il mese è febbraio, il giorno deve essere compreso tra 1 e 28 (o 29 se l'anno è bisestile)
+                        {
+                            if (giorno >= 1 && giorno <= 31)
+                            {
+                                giornoValido = true;
+                            }
+                            else
+                            {
+                                Console.Write("Giorno non valido. Inserire un giorno compreso tra 1 e 31: ");
+                                giorno = int.Parse(Console.ReadLine());
+                            }
+                        }
                         Console.Write("Inserire l'ora dell'attività: ");
                         int ora = int.Parse(Console.ReadLine());
                         Console.Write("Inserire i minuti dell'attività: ");
                         int minuti = int.Parse(Console.ReadLine());
 
-                        // DA FARE: il mese deve essere preceduto da 0 se è ad una sola cifra
-                        // (es.: febbraio deve essere stampato come "02", non "2")
-                        // altrimenti l'ordinamento per ordine alfabetico non funziona
                         string stringaData = anno + "." + mese + "." + giorno + "-" + ora + ":" + minuti;
                         string nuovaAttivita = stringaData + " | " + titolo + " | " + descrizione;
-                        
+
                         bool inserimentoRiuscito = inserisci(attivita, nuovaAttivita);
                         if (inserimentoRiuscito)
                         {
-                            Console.WriteLine("Attività inserita con successo");
+                            Console.WriteLine("Attività inserita con successo!");
                         }
                         else
                         {
@@ -66,18 +82,36 @@ namespace AgendaPersonale
                         }
                         break;
                     case "3":
-                        Console.WriteLine("DEBUG: Ordinemanto delle attività.");
-                        // Ordinamento delle attività per data (che vuol dire, per come è stata memorizzata la data, in ordine alfabetico)
+                        ordina(attivita);
+                        Console.WriteLine("Attività ordinate con successo!");
                         break;
+
                     case "4":
-                        Console.WriteLine("DEBUG: Eliminazione di un'attività.");
-                        // Eliminazione di un'attività, identificata tramite la sua posizione nell'array (l'utente la conosce perché la visualizzazione viene effettuata al punto 1)
-                        // ATTENZIONE: gestire correttamente la modifica degli indici o i "buchi" dopo l'eliminazione
+                        visualizza(attivita);
+                        Console.Write("Inserire l'indice dell'attività da eliminare: ");
+                        int indice = int.Parse(Console.ReadLine());
+
+                        if (indice >= 0 && indice < attivita.Length && attivita[indice] != null)
+                        {
+                            for (int i = indice; i < attivita.Length - 1; i++)
+                            {
+                                attivita[i] = attivita[i + 1];
+                            }
+                            attivita[attivita.Length - 1] = null;
+
+                            Console.WriteLine("Attività eliminata con successo!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Indice non valido.");
+                        }
                         break;
+
                     case "0":
                         Console.WriteLine("Bye bye!");
                         continua = false;
                         break;
+
                     default:
                         Console.WriteLine("Scelta non valida.");
                         break;
@@ -87,6 +121,7 @@ namespace AgendaPersonale
 
         static void stampaMenu()
         {
+            Console.WriteLine();
             Console.WriteLine("---------------------------");
             Console.WriteLine("1 - Visualizza le attività");
             Console.WriteLine("2 - Inserisci attività");
@@ -101,13 +136,11 @@ namespace AgendaPersonale
             {
                 if (array[i] != null)
                 {
-                    Console.WriteLine(array[i]);
+                    Console.WriteLine($"- {array[i]}");
                 }
             }
         }
 
-        // Inserisce un valore nell'array e restituisce TRUE se l'inserimento è andato a buon fine.
-        // Se viene restituito FALSE, o elem è null oppure l'array è già pieno.
         static bool inserisci(string[] array, string elem)
         {
             if (elem != null)
@@ -116,7 +149,6 @@ namespace AgendaPersonale
                 {
                     if (array[i] == null)
                     {
-                        // inserisco l'elemento
                         array[i] = elem;
                         return true;
                     }
@@ -124,5 +156,27 @@ namespace AgendaPersonale
             }
             return false;
         }
+
+        // Errore perche gli array sono delle stringhe e non puo confrontarle con i segni maggiore/minore
+        /*
+        static void ordina(string[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] != null && array[j] != null)
+                    {
+                        if (array[i] > array[j])
+                        {
+                            string temp = array[i];
+                            array[i] = array[j];
+                            array[j] = temp;
+                        }
+                    }
+                }
+            }
+        }
+        */
     }
 }
